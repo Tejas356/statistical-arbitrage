624b463ba7b06bc90c71642af5152413052d3703
Tejas <tm2723@ic.ac.uk>
2024-07-21 01:31:36 +0100

    Configured for Heroku deployment

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ def calculate():
         }
     return jsonify(result=formatted_result)
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    debug = os.environ.get("DEBUG", "False") == "True"
+    app.run(host='0.0.0.0', port=port, debug=debug)
